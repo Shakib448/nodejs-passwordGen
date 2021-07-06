5891f2b377434521c20ced2399cc324bfe243b0a
Shakiba448 <shakiba448@gmail.com>
2021-07-06 11:27:12 +0600

    PassGen

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const program = require("commander");
 
 program.version("1.0.0").description("Simple Password generator");
 
-program.option("-l", "--length <length>", "length of password").parse();
+program.option("-l, --length <number>", "length of password").parse();
+
+console.log(program.opts());
