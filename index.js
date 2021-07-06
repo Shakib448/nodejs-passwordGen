97fd21cd9600e3e04af2ab95d45ef207f219367a
Shakiba448 <shakiba448@gmail.com>
2021-07-06 11:33:37 +0600

    PassGen

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const program = require("commander");
+const createPassword = require("./utils/createPassword");
 
 program.version("1.0.0").description("Simple Password generator");
 
-program.option("-l, --length <number>", "length of password").parse();
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+  .parse();
 
-console.log(program.opts());
+const { length, save, numbers, symbols } = program.opts();
+
+// Get generated password
+
+const generatedPassword = createPassword(length, numbers, symbols);
+
+console.log(generatedPassword);
